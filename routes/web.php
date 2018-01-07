15488df4bd6862425d145fbaa9c148834594a359
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-01-07 13:01:33 -0500

    setup socialite steam provider

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/steam', 'Auth\LoginController@redirectToProvider');
+Route::get('login/steam/callback', 'Auth\LoginController@handleProviderCallback');
