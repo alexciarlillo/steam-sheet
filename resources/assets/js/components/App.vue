9d4aac63f1c770716dea0be9abf3688a7167b147
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-01-09 19:45:33 -0500

    add main container

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -2,8 +2,9 @@
     <div v-if="user">
         <Header :app="app" :user="user"></Header>
 
-        <div class="flex p-4">
+        <div class="flex flex-wrap p-4">
             <FriendsList></FriendsList>
+            <MainContainer></MainContainer>
             <UserGamesList></UserGamesList>
         </div>       
     </div>
@@ -15,10 +16,11 @@
     import Header from './Header.vue';
     import FriendsList from './FriendsList.vue';
     import UserGamesList from './UserGamesList.vue';
+    import MainContainer from './MainContainer';
 
     export default {
         computed: mapState(['user', 'app']),
 
-        components: { FriendsList, Header, UserGamesList }
+        components: { FriendsList, Header, UserGamesList, MainContainer }
     }
 </script>
---
+++ resources/assets/js/components/MainContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-3/5 px-2">
+        <div class="rounded overflow-hidden shadow-lg bg-white h-64">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+    export default {
+        name: 'MainContainer',
+
+        props: [],
+
+        computed: {
+        },
+
+        components: {
+        }
+    }
+</script>
